e6b69d754241d0c08fea8777b8e306c2910c9434
Ham Shafi <hamid.shafikani@gmail.com>
2018-08-23 15:57:26 +0200

    here first Git

---
+++ src/main/java/com/spring/springAOP/telusko/Helper.java
@@ -0,0 +1,20 @@
+package com.spring.springAOP.telusko;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+// advice --> what ? 1.before (before calling show) 2. after 3. around
+//PointCut --> where ?
+@Component
+@Aspect
+@EnableAspectJAutoProxy
+public class Helper   {
+
+    @Before("execution(public void show())")
+    public void log() {
+        System.out.println("show called log()");
+    }
+}
